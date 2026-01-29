73441b34f64c7b9785a8202324c5cc042339f5ac
Melker <melker.stjarnqvist1998@hotmail.com>
2026-01-29 20:13:53 +0100

    Add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import "./globals.css";
+import Script from "next/script";
+
 import { ThemeProvider } from "next-themes";
 import Navbar from "@/app/components/layout/Navbar";
 import Footer from "@/app/components/layout/Footer";
 import { Toaster } from "sonner";
+import { SessionProvider } from "next-auth/react";
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 export default function RootLayout({
   children,
@@ -12,24 +17,57 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="sv" suppressHydrationWarning>
-      <body className="min-h-screen flex flex-col bg-gray-50 text-gray-900 dark:bg-[#0a0f1c] dark:text-gray-100 transition-colors duration-500 ease-in-out">
-        {/* ✅ ThemeProvider ska ligga här, runt allt i BODY – inte på HTML-nivå */}
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-          {/* Navbar */}
-          <Navbar />
+    <html
+      lang="sv"
+      suppressHydrationWarning
+      className="bg-gray-50 text-gray-900 dark:bg-[#0a0f1c] dark:text-gray-100 transition-colors duration-500 ease-in-out"
+    >
+      <head>
+        {GTM_ID && (
+          <Script
+            id="gtm"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');
+              `,
+            }}
+          />
+        )}
+      </head>
 
-          {/* Main content */}
-          <main className="flex-1 pt-20 pb-16 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-6xl mx-auto">{children}</div>
-          </main>
+      <body className="min-h-screen flex flex-col">
+        {GTM_ID && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
 
-          {/* Footer */}
-          <Footer />
+        <SessionProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem={false}
+          >
+            <Navbar />
 
-          {/* Toaster för notiser */}
-          <Toaster richColors position="bottom-center" />
-        </ThemeProvider>
+            <main className="flex-1 pt-20 pb-16 px-4 sm:px-6 lg:px-8">
+              <div className="max-w-6xl mx-auto">{children}</div>
+            </main>
+
+            <Footer />
+            <Toaster richColors position="bottom-center" />
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
